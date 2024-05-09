f57040a1585a460e2f1203a1a9ea547aa0bc329c
Matheus Madureira <matheus_mmadu@hotmail.com>
2024-05-09 18:39:02 -0300

    Classe main para execução de teste das classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Iphone - **FEITO**
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1212341", modelo: "modelo 1", imei: "1111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Wpp");
+
+Console.WriteLine("");
+
+Console.WriteLine("Smartphone IPhone: ");
+Smartphone iphone = new Iphone(numero: "67896767", modelo: "modelo 2", imei: "2222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
